d0628638887ddb1026bb986c75007b3281ad30a8
J.guzman <61435395+jguzman11@users.noreply.github.com>
2020-03-23 14:34:40 -0400

    Week 5/6 challenges

---
+++ HARD-JAVASCRIPT/script.js
@@ -0,0 +1,14 @@
+var year, yearJohn, yearMark;
+now = 2018
+ageJohn = 28;
+ageMark = 33;
+
+yearJohn = now - 28;
+yearMark = now - 33;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 2);
+console.log(now - 2);
