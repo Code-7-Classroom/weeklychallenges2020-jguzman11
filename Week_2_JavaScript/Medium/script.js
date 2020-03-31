090a2d753ada6324dc48a5464c14886f9833b3f1
jguzman11 <61435395+jguzman11@users.noreply.github.com>
2020-03-30 23:48:32 -0400

    Add files via upload

---
+++ Week_2_JavaScript/Medium/script.js
@@ -0,0 +1,41 @@
+var months = ['Jan','Feb','Mar','Apr','May','June','July','Aug','Sep','Oct','Nov','Dec'];
+var i = parseInt(prompt("enter a number"));
+
+switch (i){
+    case 1:
+        console.log(months[0])
+        break;
+    case 2:
+        console.log(months[1])
+        break;
+    case 3:
+        console.log(months[2])
+        break;
+    case 4:
+        console.log(months[3])
+        break;
+    case 5:
+        console.log(months[4])
+        break;
+    case 6:
+        console.log(months[5])
+        break;
+    case 7:
+        console.log(months[6])
+        break;
+    case 8:
+        console.log(months[7])
+        break;
+    case 9:
+        console.log(months[8])
+        break;
+    case 10:
+        console.log(months[9])
+        break;
+    case 11:
+        console.log(months[10])
+        break;
+    case 12:
+        console.log(months[11])
+        break;
+}
