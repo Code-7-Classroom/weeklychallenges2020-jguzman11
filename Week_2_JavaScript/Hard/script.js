b1ffc30884e6c7bba9dfa08e7b349ee99b1fac10
J.guzman <61435395+jguzman11@users.noreply.github.com>
2020-03-30 23:46:56 -0400

    Ecommerce

---
+++ Week_2_JavaScript/Hard/script.js
@@ -0,0 +1,11 @@
+var tom, jerry, tomBMI, jerryBMI;
+tom = [9, 8];
+jerry = [3.937010, .099208]
+jerryBMI = (jerry[1]/(jerry[0]*jerry[0]));
+tomBMI = (tom[1]/(tom[0]*tom[0]));
+
+tomBMI<jerryBMI == false
+
+if(!false){
+    console.log('Is Toms BMI higher than Jerrys? ' + false)
+}
